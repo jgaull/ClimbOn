3140ac02cdfb5b8e8a3cb507d0f1d9da566ff7b8
Grant H <ghelton@gmail.com>
2013-03-02 20:10:16 -0800

    cloud code

---
+++ CloudCode/cloud/main.js
@@ -0,0 +1,8 @@
+	
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+// Parse.Cloud.beforeSave("Post", function(request, response) {
+// 	var tags = request.object.('tags').match(/(^|\s)#([^ ]*)/g);
+
+// 	response.success();
+// });
