8860c5f07ce1c92e45d1ef9ea8b245d1b20a23d2
Jonathan Gaull <jonbeesh@gmail.com>
2013-03-09 13:38:15 -0800

    Added the more comments cell class.

---
+++ ClimbOn/MoreCommentsCell.h
@@ -0,0 +1,13 @@
+//
+//  MoreCommentsCell.h
+//  ClimbOn
+//
+//  Created by Jonathan Gaull on 3/9/13.
+//  Copyright (c) 2013 OneHeadedLlama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreCommentsCell : UITableViewCell
+
+@end
