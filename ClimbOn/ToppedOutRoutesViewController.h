ddde64ae42f9b94a909d2a03ff9181d26fb9f412
Jonathan Gaull <jonbeesh@gmail.com>
2013-04-21 11:56:03 -0700

    Got recently topped out routes loading. I'm pretty sure this feature is totally useless.

---
+++ ClimbOn/ToppedOutRoutesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToppedOutRoutesViewController.h
+//  ClimbOn
+//
+//  Created by Jonathan Gaull on 4/21/13.
+//  Copyright (c) 2013 OneHeadedLlama. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface ToppedOutRoutesViewController : PFQueryTableViewController
+
+@end
